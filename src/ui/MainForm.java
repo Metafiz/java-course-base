e686d3f6f66726564be1cb9bbefb95c002443d37
Airat Galyamov <galyamov.airat@gmail.com>
2020-11-25 12:48:45 +0500

    отрисовка фигур

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -1,10 +1,17 @@
 package ui;
 
+import classes.figures.Circle;
+import classes.figures.Rectangle;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
-    private JButton button1;
     private JPanel rootPanel;
+    private JButton btnAddRect;
+    private JButton btnAddCircle;
+    private JPanel paintPanel;
 
     public MainForm() {
         setContentPane(rootPanel);
@@ -13,6 +20,26 @@ public class MainForm extends JFrame {
         this.setSize(800, 600);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        btnAddCircle.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Circle c = new Circle(1, 300, 200, 80);
+                c.paint(paintPanel.getGraphics());
+            }
+        });
+
+        btnAddRect.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                drawRect();
+            }
+        });
+    }
+
+    private void drawRect() {
+        Rectangle rect = new Rectangle(1, 50, 50, 250, 200);
+        rect.paint(paintPanel.getGraphics());
     }
 
     public static void main(String[] args) {
