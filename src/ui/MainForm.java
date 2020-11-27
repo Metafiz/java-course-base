1155cae27bed6f1af76786f896cc85c6e408044d
Airat Galyamov <galyamov.airat@gmail.com>
2020-11-27 14:31:45 +0500

    диалог выбора файла

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -8,6 +8,7 @@ import classes.figures.Rectangle;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MainForm extends JFrame {
     private JPanel rootPanel;
@@ -36,7 +37,19 @@ public class MainForm extends JFrame {
         btnAddRect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                drawRect();
+                //drawRect();
+                final JFileChooser fc = new JFileChooser();
+                if (true) {
+                    int returnVal = fc.showOpenDialog(MainForm.this);
+
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        //This is where a real application would open the file.
+                        System.out.println("Opening: " + file.getAbsolutePath() + ".\n");
+                    } else {
+                        System.out.println("Open command cancelled by user.\n");
+                    }
+                }
             }
         });
 
